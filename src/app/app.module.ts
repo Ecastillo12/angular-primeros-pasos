1339992ed418d256d252ccd1d5a3c9bab570e489
Eduardo Salvador Castillo <e.castillo4@dxc.com>
2023-11-13 20:18:31 -0600

    ngIf, ngFor, ngTemplate y ngIf-else

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
-import { CounterComponent } from './counter/counter.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    CounterComponent,
     HeroComponent,
-    ListComponent,
-    CounterComponent
+    ListComponent
   ],
   imports: [
     BrowserModule,
